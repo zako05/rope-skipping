c58d5742884f25f0c469f8399630f442aa46898c
test <test@test.com>
2015-05-15 15:19:47 +0200

    added new slider 'slippry' without styling yet

---
+++ assets/common/js/slippry_banner.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+	var slider_images = $("#slider_images").slippry({
+		// transition: 'fade',
+		//useCSS: true,
+		// speed: 1000,
+		// pause: 3000,
+		//auto: true,
+		// preload: 'visible',
+		//autoHover: false
+	});
+
+});
