f0d25f80d2bc44d0792a5ea5f59a5b29c68e9731
zako05 <zako05@gmail.com>
2015-06-01 22:08:49 +0200

    added all files from master

---
+++ assets/common/js/slippry_banner.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+	var slider = $("#slippry-slider-img").slippry({
+		// transition: 'fade',
+		 useCSS: true,
+		// speed: 1000,
+		// pause: 3000,
+		// auto: true,
+		// preload: 'visible',
+		// autoHover: false
+		// controls: true
+	});
+
+});
