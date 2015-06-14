5304327ea617f27282ff8f5e34431ad2042adc31
zako05 <zako05@gmail.com>
2015-06-15 01:09:42 +0200

    changed max-length - not finished yet

--- assets/common/jquery/max_length.js
+++ assets/common/jquery/max_length.js
@@ -1,4 +1,10 @@
-jQuery(document).ready(function ($) {
-	var myDiv = $('.max-length');
+var item = ['.max-length-training','.max-length-notice','.max-length-ropeskipping'];
+
+//jQuery(document).ready(function($) {
+jQuery.each(item, function(index,value) {
+
+	var myDiv = $(value);
+	console.log(myDiv);
+	
 	myDiv.text(myDiv.text().substring(0,300))
 });
